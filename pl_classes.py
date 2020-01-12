0718cb0f7218168c8dfac6583591a194a25abb34
Aaron Goodrich <aarongoodrich3@gmail.com>
2020-01-11 21:44:12 -0500

    built out parking lot classes and their unit tests

---
+++ pl_classes.py
@@ -0,0 +1,37 @@
+class ParkingLot:
+    def __init__(self,number_of_spaces):
+        self.parking_lot_spaces = {}
+        for slot in range(1, int(number_of_spaces) + 1):
+            self.parking_lot_spaces[slot] = None
+
+    def park(self, registration_number, color):
+        for slot in sorted(self.parking_lot_spaces.keys()):
+            if self.parking_lot_spaces[slot] == None:
+                self.parking_lot_spaces[slot] = Vehicle(registration_number.upper(), color.capitalize())
+                return slot
+        return None
+
+    def leave(self, spot_number):
+        print(list(self.parking_lot_spaces.keys()))
+        if int(spot_number) in list(self.parking_lot_spaces.keys()): 
+            self.parking_lot_spaces[int(spot_number)] = None
+            return True
+        else:
+            return False
+
+    def find_matches(self, s, vehicle_attr, search_for):
+        matches = []
+        for slot in sorted(self.parking_lot_spaces.keys()):
+            if getattr(self.parking_lot_spaces[slot], vehicle_attr) == s:
+                if search_for == "slot":
+                    matches.append(slot)
+                elif search_for == "registration_number":
+                    matches.append(self.parking_lot_spaces[slot].registration_number)
+                elif search_for == "color":
+                    matches.append(self.parking_lot_spaces[slot].color)
+        return matches
+
+class Vehicle:
+    def __init__(self,registration_number,color):
+        self.registration_number = registration_number
+        self.color = color
---
+++ tests/test_pl_classes.py
@@ -0,0 +1,57 @@
+import unittest
+from pl_classes import ParkingLot, Vehicle
+
+class TestParkingLotMethods(unittest.TestCase):
+
+    def test_create_parking_lot_with_expected_number_of_slots(self):
+        parking_lot_size = 6
+        parking_lot = ParkingLot(parking_lot_size)
+        self.assertEqual(len(parking_lot.parking_lot_spaces), parking_lot_size)
+        self.assertIsNone(parking_lot.parking_lot_spaces[1])
+        self.assertIsNone(parking_lot.parking_lot_spaces[parking_lot_size])
+        self.assertRaises(KeyError, lambda: parking_lot.parking_lot_spaces[0])
+        self.assertRaises(KeyError, lambda: parking_lot.parking_lot_spaces[parking_lot_size+1])
+
+    def test_park_returns_slot_key_if_slot_is_empty_and_none_if_all_slots_taken(self):
+        parking_lot_size = 2
+        parking_lot = ParkingLot(parking_lot_size)
+        park_return_variable = parking_lot.park("ka-01-hh-1234" , "white")
+        self.assertEqual(park_return_variable, 1)
+        park_return_variable = parking_lot.park("ka-01-hh-1234" , "white")
+        self.assertEqual(park_return_variable, 2)
+        park_return_variable = parking_lot.park("ka-01-hh-1234" , "white")
+        self.assertIsNone(park_return_variable)
+
+    def test_park_creates_vehicle_with_correct_variable_formatting(self):
+        parking_lot_size = 1
+        parking_lot = ParkingLot(parking_lot_size)
+        parking_lot.park("ka-01-hh-1234", "white")
+        self.assertEqual(parking_lot.parking_lot_spaces[1].registration_number, "KA-01-HH-1234")
+        self.assertEqual(parking_lot.parking_lot_spaces[1].color, "White")
+
+    def test_leave_returns_true_if_slot_exists_and_false_otherwise(self):
+        parking_lot_size = 2
+        parking_lot = ParkingLot(parking_lot_size)
+        self.assertTrue(parking_lot.leave(1))
+        self.assertTrue(parking_lot.leave(2))
+        self.assertFalse(parking_lot.leave(0))
+        self.assertFalse(parking_lot.leave(3))
+
+    def test_leave_converts_strings_to_integer(self):
+        parking_lot_size = 2
+        parking_lot = ParkingLot(parking_lot_size)
+        self.assertTrue(parking_lot.leave("1"))
+
+    def test_find_matches_returns_list_of_correct_vehicle_attributes_or_parking_slots(self):
+        parking_lot_size = 6
+        parking_lot = ParkingLot(parking_lot_size)
+        for x in range(6):
+            parking_lot.park("AK-01-HH-1234", "White")
+        self.assertEqual(parking_lot.find_matches("AK-01-HH-1234","registration_number","slot"),[1,2,3,4,5,6])
+        self.assertEqual(parking_lot.find_matches("White","color","slot"),[1,2,3,4,5,6])
+        self.assertEqual(parking_lot.find_matches("whit","color","slot"),[])
+        self.assertEqual(parking_lot.find_matches("White","color","registration_number"),["AK-01-HH-1234"]*6)
+
+
+if __name__ == '__main__':
+    unittest.main()
